9377fa1895191f9fb4978d4520d55357bd8a73a3
MMBOSTON <mr_matin@yahoo.com>
2024-05-10 18:07:12 -0400

    Updated the AgGrid feature. Table is shown. Need Selectable row option next.

--- input_proc/input_proc.py
+++ input_proc/input_proc.py
@@ -1,17 +1,5 @@
 import sys
 import os
-
-# Add the parent directory to the Python path
-current_dir = os.path.dirname(__file__)
-parent_dir = os.path.dirname(current_dir)
-sys.path.append(parent_dir)
-
-# Now you can import using absolute imports from the project root
-from src.task import Task
-from src.visualizer import Visualizer
-
-# Continue with the rest of your imports and code
-
 import json
 import pandas as pd
 import streamlit as st
@@ -19,43 +7,8 @@ import streamlit as st
 from .data_entry_gui import create_interactive_table
 from .generate_tasks import generate_core_tasks, save_tasks_to_json
 from st_aggrid import AgGrid
-
-# def display_tasks_with_aggrid(tasks):
-#     task_dicts = [
-#         {
-#             "Name": task.name,
-#             "Description": task.description,
-#             "Value": task.value,
-#             "Effort": task.effort,
-#             "ID": task.id,
-#         }
-#         for task in tasks
-#     ]
-
-#     gridOptions = {
-#         'enableSorting': True,
-#         'enableFilter': False,
-#         #'rowSelection': 'multiple',  # Enable multiple row selection
-#         'defaultColDef': {
-#             'sortable': True,
-#             'filter': False,
-#             'resizable': True,
-#             'width': 100,
-#         },
-#         'columnDefs': [
-#             {'headerName': "Task Name", 'field': "Name", 'width': 150},
-#             {'headerName': "Task Description", 'field': "Description", 'width': 300},
-#             {'headerName': "Task Value", 'field': "Value", 'width': 100, 'cellRenderer': 'agProgressBarCellRenderer'},
-#             {'headerName': "Task Effort", 'field': "Effort", 'width': 100},
-#             {'headerName': "Task ID", 'field': "ID", 'width': 100, 'resizable': False},
-#         ],
-#         'getRowStyle': lambda params: {'background-color': '#f4f4f4'} if params.node.rowIndex % 2 else {}
-#     }
-
-#     response = AgGrid(pd.DataFrame(task_dicts), gridOptions=gridOptions, return_mode='AS_INPUT', update_mode='VALUE_CHANGED')
-
-#     selected_rows = response['selected_rows']
-#     return selected_rows
+from src.task import Task
+from src.visualizer import Visualizer
 
 def display_tasks_with_aggrid(tasks):
     task_dicts = [
@@ -69,10 +22,8 @@ def display_tasks_with_aggrid(tasks):
         for task in tasks
     ]
 
-    response = AgGrid(pd.DataFrame(task_dicts), return_mode='AS_INPUT', update_mode='VALUE_CHANGED')
-
-    selected_rows = response['selected_rows']
-    return selected_rows
+    grid_response = AgGrid(pd.DataFrame(task_dicts))
+    return grid_response
 
 def display_tasks_with_st_table(tasks):
     task_dicts = [
@@ -90,6 +41,7 @@ def display_tasks_with_st_table(tasks):
 
 def InputProc():
     visualizer = Visualizer()
+    tasks = []
 
     with st.expander("Task Management"):
         col1, col2 = st.columns(2)
@@ -120,18 +72,15 @@ def InputProc():
                 tasks = [Task(description=task_dict['description'], value=task_dict['value'], effort=task_dict['task_effort'], id=task_dict['task_id'], name=task_dict['name']) for task_dict in task_dicts]
         except FileNotFoundError:
             st.warning("No tasks found. Generate some or use sample data.")
-            tasks = []
         except json.JSONDecodeError:
             st.error("Failed to decode JSON file. Please check the file content.")
-            tasks = []
         except KeyError as e:
             st.error(f"Failed to create tasks. Missing key in dictionary: {e}")
-            tasks = []
 
     # Display the tasks with ag-grid or st.table based on user's choice
     use_ag_grid = st.checkbox('Use AgGrid')
     if use_ag_grid:
-        selected_rows = display_tasks_with_aggrid(tasks)
+        grid_response = display_tasks_with_aggrid(tasks)
     else:
         display_tasks_with_st_table(tasks)
 
